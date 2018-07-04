6ca05c0bb1c2e8ad28212d05ed65f2fcf6ac389e
Dave Kerr <dave@havenlife.com>
2018-07-04 14:19:29 -0400

    [1.5] create READ/PRINT/LOOP portions of REPL to output tokens

---
+++ repl/repl.go
@@ -0,0 +1,34 @@
+// repl/repl.go
+
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkeylang/lexer"
+	"monkeylang/token"
+)
+
+// PROMPT defines the REPL prompt characters
+const PROMPT = ">> "
+
+// Start will execute the READ-EVAL-PRINT-LOOP (REPL)
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
